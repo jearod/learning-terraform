23ad13d469d4b4dd82c4e06f70d46ac4b20a4e7a
Jean Rodriguez <frankrodriguez9517@gmai.com>
2025-01-28 12:12:20 -0500

    update main.tf

--- main.tf
+++ main.tf
@@ -14,10 +14,26 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
-
+  subnet_id     = aws_subnet.main.id
   tags = {
     Name = "HelloWorld"
   }

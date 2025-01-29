a8e07f1ba796c08351cd742f45569fe8ebcc81d0
Jean Rodriguez <frankrodriguez9517@gmai.com>
2025-01-29 12:22:08 -0500

    lab2 sg module

--- main.tf
+++ main.tf
@@ -38,55 +38,71 @@ data "aws_subnet" "default" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.security_group.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
 
-resource "aws_security_group" "blog" {
-  name       = "blog-sg"
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
 
-  tags = {
-    Name = "blog-sg"
-  }
-
+  name    = "blog-sg"
   vpc_id = data.aws_vpc.default.id
+
+  ingress_rules = ["https-443-tcp","http-80-tcp","ssh-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]  
-  security_group_id = aws_security_group.blog.id
-}
 
-resource "aws_security_group_rule" "blog_https_in" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       =  ["0.0.0.0/0"]  
-  security_group_id = aws_security_group.blog.id
-}
 
-resource "aws_security_group_rule" "blog_ssh_in" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       =  ["0.0.0.0/0"]  
-  security_group_id = aws_security_group.blog.id
-}
+# resource "aws_security_group" "blog" {
+#   name       = "blog-sg"
 
-resource "aws_security_group_rule" "blog_everything_out" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]  
-  security_group_id = aws_security_group.blog.id
-}
+#   tags = {
+#     Name = "blog-sg"
+#   }
+
+#   vpc_id = data.aws_vpc.default.id
+# }
+
+# resource "aws_security_group_rule" "blog_http_in" {
+#   type              = "ingress"
+#   from_port         = 80
+#   to_port           = 80
+#   protocol          = "tcp"
+#   cidr_blocks       = ["0.0.0.0/0"]  
+#   security_group_id = aws_security_group.blog.id
+# }
+
+# resource "aws_security_group_rule" "blog_https_in" {
+#   type              = "ingress"
+#   from_port         = 443
+#   to_port           = 443
+#   protocol          = "tcp"
+#   cidr_blocks       =  ["0.0.0.0/0"]  
+#   security_group_id = aws_security_group.blog.id
+# }
+
+# resource "aws_security_group_rule" "blog_ssh_in" {
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   cidr_blocks       =  ["0.0.0.0/0"]  
+#   security_group_id = aws_security_group.blog.id
+# }
+
+# resource "aws_security_group_rule" "blog_everything_out" {
+#   type              = "egress"
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   cidr_blocks       = ["0.0.0.0/0"]  
+#   security_group_id = aws_security_group.blog.id
+# }

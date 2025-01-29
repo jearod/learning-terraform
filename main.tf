f67a89648121579aa3c146d078249ab3ab719f4c
Jean Rodriguez <frankrodriguez9517@gmai.com>
2025-01-29 13:52:10 -0500

    lab3 vpc module

--- main.tf
+++ main.tf
@@ -14,30 +14,47 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+# data "aws_vpc" "default" {
+#   default = true
+# }
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "blog_vpc_dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
 
-data "aws_subnet" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
+# data "aws_subnet" "default" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [data.aws_vpc.default.id]
+#   }
 
-  filter {
-    name   = "default-for-az"
-    values = ["true"]
-  }
+#   filter {
+#     name   = "default-for-az"
+#     values = ["true"]
+#   }
   
-  filter {
-    name   = "availability-zone"
-    values = ["us-east-1a"] # Specify the AZ you want to use
-  }
-}
+#   filter {
+#     name   = "availability-zone"
+#     values = ["us-east-1a"] # Specify the AZ you want to use
+#   }
+# }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.security_group.security_group_id]
 
   tags = {
@@ -50,15 +67,13 @@ module "security_group" {
   version = "5.3.0"
 
   name    = "blog-sg"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id  = module.vpc.vpc_id
 
-  ingress_rules = ["https-443-tcp","http-80-tcp","ssh-tcp"]
+  ingress_rules       = ["https-443-tcp","http-80-tcp","ssh-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules = ["all-all"]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
-
 
 
 # resource "aws_security_group" "blog" {
